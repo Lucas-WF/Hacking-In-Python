4c21db1d24abdc10faa8aecc654c43913867d951
Lucas-WF <lucas.wf@outlook.com>
2021-08-23 17:20:22 -0300

    Updating the basic_portscan.py

--- basic_portscan/basic_portscan.py
+++ basic_portscan/basic_portscan.py
@@ -2,20 +2,20 @@ import pyfiglet
 import sys
 import socket
 
-ascii_banner = pyfiglet.figlet_format("PORTSCAN")
+ascii_banner = pyfiglet.figlet_format("PY PORTSCAN")
 print(ascii_banner)  # Printing the banner
 
-ip = socket.gethostbyname(sys.argv[1]) if len(sys.argv) > 1 else print(66 * "*" + "\nTYPE THE IP!! Exiting..\n" + 66 * "*") and sys.exit()
+ip = socket.gethostbyname(sys.argv[1]) if len(sys.argv) > 1 else print(66 * "*" + "\nTYPE THE IP!! Exiting..\n" + 66 * "*") and sys.exit()  # If the first arg is not defined
 
 if ip is not None:
     print(66 * "*")
-    print("SCANNING HOST: " + str(ip))
+    print("SCANNING HOST: " + str(ip))  # Showing the Scan process
     print(66 * "*")
-    try:
-        for ports in range(1, 65535):
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socket.setdefaulttimeout(1)
-            if s.connect_ex((ip, ports)) == 0:
+    try:  # To cover errors or not
+        for ports in range(1, 65535):  # For all ports
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Creating a socket
+            socket.setdefaulttimeout(1)  # Setting a timeout
+            if s.connect_ex((ip, ports)) == 0:  # If the ports are open
                 print(socket.getservbyport(ports).upper() + f" - Port {ports} open")
             s.close()
 
@@ -25,7 +25,7 @@ if ip is not None:
     except socket.gaierror:
         print("No host defined!")
         sys.exit()
-    except socket.timeout:
+    except socket.timeout:  #Never occurs because we defined a timeout
         print("Timeout Error")
         sys.exit()
     except socket.error:
