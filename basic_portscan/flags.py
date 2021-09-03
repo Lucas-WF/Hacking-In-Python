450d0f5a06c058f5fc55bc49da98e4e11e07630b
Lucas-WF <lucas.wf@outlook.com>
2021-09-03 16:47:01 -0300

    Fixing flags.py

--- basic_portscan/flags.py
+++ basic_portscan/flags.py
@@ -4,7 +4,7 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', dest="min_port", action="store", help="Define the low port", type=int, default=1)
 parser.add_argument('-pe',dest="max_port", action="store", help = "Define the high port", type=int, default=1000)
-if len(sys.argv) > 1 and sys.argv[1] == '-h':
+if len(sys.argv) > 1 and sys.argv[1] == '-h' or len(sys.argv) > 1 and sys.argv[1] == "--help":
     args = parser.parse_args()
 else:
     args = parser.parse_args(sys.argv[2:])
