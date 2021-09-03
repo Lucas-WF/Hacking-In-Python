8f7a81e667cb05162746bdf004e5649be2c2dbdf
Lucas-WF <lucas.wf@outlook.com>
2021-09-03 17:23:30 -0300

    Updating the flags.py

--- basic_portscan/flags.py
+++ basic_portscan/flags.py
@@ -6,9 +6,9 @@ parser.add_argument('-p', dest="min_port", action="store", help="Define the low 
 parser.add_argument('-pe',dest="max_port", action="store", help = "Define the high port", type=int, default=1000)
 if len(sys.argv) > 1 and sys.argv[1] == '-h' or len(sys.argv) > 1 and sys.argv[1] == "--help":
     args = parser.parse_args()
+elif __name__ == '__main__':
+    args = parser.parse_args()
+    print("This is the file who contains the flags of the Port Scan")
+    print(f"The min port is {args.min_port}, the max port is {args.max_port}")
 else:
     args = parser.parse_args(sys.argv[2:])
-
-if __name__ == '__main__':
-    print("This is the file who contains the flags of the Port Scan")
-    print(args.min_port, args.max_port)
