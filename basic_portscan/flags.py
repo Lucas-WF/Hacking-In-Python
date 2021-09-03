70377e9656312a53c842fff6407365050f0f94f8
Lucas-WF <lucas.wf@outlook.com>
2021-09-03 16:27:04 -0300

    Fixing flags.py

--- basic_portscan/flags.py
+++ basic_portscan/flags.py
@@ -4,7 +4,10 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', dest="min_port", action="store", help="Define the low port", type=int, default=1)
 parser.add_argument('-pe',dest="max_port", action="store", help = "Define the high port", type=int, default=1000)
-args = parser.parse_args()
+if len(sys.argv) > 1 and sys.argv[1] == '-h':
+    args = parser.parse_args()
+else:
+    args = parser.parse_args(sys.argv[2:])
 
 if __name__ == '__main__':
     print("This is the file who contains the flags of the Port Scan")
