daf174e7274092f8b755d337ecfb0c3bf3c17663
Lucas-WF <lucas.wf@outlook.com>
2021-09-03 21:57:35 -0300

    Adding the usage, credits and others to flags.py

--- basic_portscan/flags.py
+++ basic_portscan/flags.py
@@ -1,7 +1,7 @@
 import argparse
 import sys
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(prog="basic_portscan.py", description="Basic Python Portscan 1.0 (https://github.com/Lucas-WF/Hacking-In-Python)", usage="%(prog)s [target] {options}", epilog="Credits to Lucas Wagner Fernandes")
 parser.add_argument('-p', dest="min_port", action="store", help="Define the low port", type=int, default=1)
 parser.add_argument('-pe',dest="max_port", action="store", help = "Define the high port", type=int, default=1000)
 if len(sys.argv) > 1 and sys.argv[1] == '-h' or len(sys.argv) > 1 and sys.argv[1] == "--help":
